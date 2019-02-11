68f2ebbe6cbd90c5493d83f7be3eaa803941b981
JBorrow <joshua.borrow@durham.ac.uk>
2019-02-11 15:01:10 +0000

    Added 1 thread

--- make_batch.sh
+++ make_batch.sh
@@ -6,7 +6,7 @@ for version in 0.5.0 0.6.0 0.7.0 0.8.0;
 do
     mkdir $version
     cd $version
-    for thread in 2 4 7 14;
+    for thread in 1 2 4 7 14;
     do
         cp ../../submit_template.slurm "./submit_${thread}.slurm"
         sed -i "s/THREADS/${thread}/g" "./submit_${thread}.slurm"
