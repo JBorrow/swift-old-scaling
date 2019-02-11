8c077fcb412121d649a3b8bdb1a0e2893ec263ad
JBorrow <joshua.borrow@durham.ac.uk>
2019-02-11 20:19:03 +0000

    Changed y-axis units

--- make_plot.py
+++ make_plot.py
@@ -8,13 +8,13 @@ def get_time(filename):
     first_try = np.loadtxt(filename, usecols=-2).sum()
 
     if first_try:
-        return first_try
+        return first_try / (60 * 1000)
     else:
         # Wrong column to read
-        return np.loadtxt(filename, usecols=-1).sum()
+        return np.loadtxt(filename, usecols=-1).sum() / (60 * 1000)
 
 versions = [f"0.{x}.0" for x in [5, 6, 7, 8]]
-threads = [4, 8]
+threads = [2, 4, 8, 14, 28]
 output = [[get_time(f"output/{v}/timesteps_{t}.txt") for t in threads] for v in versions]
 
 plt.loglog()
@@ -22,8 +22,10 @@ plt.loglog()
 for version, times in zip(versions, output):
     plt.plot(threads, times, label=version)
 
+plt.xticks(threads, threads)
+
 plt.legend()
 plt.xlabel("Threads")
-plt.ylabel("Runtime [ms]")
+plt.ylabel("Runtime [mins]")
 
 plt.savefig("runtime_scaling.pdf")
