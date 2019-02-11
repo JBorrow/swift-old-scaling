1b895a16ecaa0aa85681064104ec2ddb9d41f175
JBorrow <joshua.borrow@durham.ac.uk>
2019-02-11 15:00:58 +0000

    Added a commenting out of engine_dump_snapshot

--- compile_and_move.sh
+++ compile_and_move.sh
@@ -9,6 +9,10 @@ mkdir logs
 for version in 0.5.0 0.6.0;
 do
     cd "swiftsim-v${version}"
+
+    # Comment out engine_dump_snapshot
+    sed -i "s/engine_dump_snapshot(e);//g" src/engine.c
+
     ./autogen.sh
     ./configure --disable-vec --with-tbbmalloc > "../logs/configure_log_${version}"
     make -j > "../logs/make_log${version}"
@@ -21,6 +25,10 @@ done
 for version in 0.7.0 0.8.0;
 do
     cd "swiftsim-v${version}"
+
+    # Comment out engine_dump_snapshot
+    sed -i "s/engine_dump_snapshot(e);//g" src/engine.c
+
     ./autogen.sh
     ./configure --with-tbbmalloc > "../logs/configure_log_${version}"
     make -j > "../logs/make_log${version}"
